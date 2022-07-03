cc9792345880820688663529cb94aea30b88c9fc
Cihan Sungur <sungur1988@gmail.com>
2022-07-03 21:44:45 +0300

    StockUnitsControllerCreatedWithCreateMethod

---
+++ src/Api/WebApi/Controllers/StockUnitsController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.StockUnits.Requests.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockUnitsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StockUnitsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateStockUnit(CreateStockUnitCommand request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+    }
+}
