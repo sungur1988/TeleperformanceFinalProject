e8a2e870856b0682458960a23bba5353b2c276b4
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 00:04:51 +0300

    DeleteStockUnitCommandCreated

---
+++ src/Core/Application/Features/StockUnits/Requests/Commands/DeleteStockUnitCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Wrapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.StockUnits.Requests.Commands
+{
+    public record DeleteStockUnitCommand(string Id):IRequest<BaseResponse>;
+}
