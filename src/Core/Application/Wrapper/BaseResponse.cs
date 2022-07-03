6841aa3289ca1efa6d1c3985399403949484e9f3
Cihan Sungur <sungur1988@gmail.com>
2022-07-03 20:30:01 +0300

    BaseResponseCreated

---
+++ src/Core/Application/Wrapper/BaseResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Wrapper
+{
+    public class BaseResponse
+    {
+        public BaseResponse(bool isSuccess,int statusCode)
+        {
+            IsSuccess = isSuccess;
+            StatusCode = statusCode;
+        }
+        public BaseResponse(bool isSuccess,int statusCode, string message) : this(isSuccess,statusCode)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+
+        public bool IsSuccess { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
