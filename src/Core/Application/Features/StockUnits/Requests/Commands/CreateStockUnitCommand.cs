3acffb05cc4081af6ef7cea1000c935c8fd4fe5f
Cihan Sungur <sungur1988@gmail.com>
2022-07-03 21:45:57 +0300

    CreateStockUnitCommandCreated

---
+++ src/Core/Application/Features/StockUnits/Requests/Commands/CreateStockUnitCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos;
+using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.StockUnits.Requests.Commands
+{
+    public record CreateStockUnitCommand(string Name) : IRequest<ServiceResponse<StockUnitDto>>;
+}
