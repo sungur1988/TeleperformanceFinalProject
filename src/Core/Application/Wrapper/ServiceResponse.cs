fd8fbb42be417511127af8de888b0ac41d9f2d50
Cihan Sungur <sungur1988@gmail.com>
2022-07-03 20:30:23 +0300

    ServiceResponseCreated

---
+++ src/Core/Application/Wrapper/ServiceResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Wrapper
+{
+    public class ServiceResponse<T> : BaseResponse
+    {
+        public T Value { get; set; }
+        public ServiceResponse(T value, bool isSuccess, int statusCode,string message) : base(isSuccess,statusCode,message)
+        {
+            Value = value;
+        }
+
+        public ServiceResponse(T value, bool isSuccess,int statusCode) : base(isSuccess,statusCode)
+        {
+            Value = value;
+        }
+    }
+}
