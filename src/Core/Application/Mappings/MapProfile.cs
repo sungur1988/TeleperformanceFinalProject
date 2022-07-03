7f83b4fa3f8c9fda9065891b0bd2445aa1f5a624
Cihan Sungur <sungur1988@gmail.com>
2022-07-03 21:46:21 +0300

    MapProfileAdded

---
+++ src/Core/Application/Mappings/MapProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.Dtos;
+using Application.Features.StockUnits.Requests.Commands;
+using AutoMapper;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappings
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<StockUnit, CreateStockUnitCommand>().ReverseMap();
+            CreateMap<StockUnit, StockUnitDto>().ReverseMap();
+        }
+    }
+}
