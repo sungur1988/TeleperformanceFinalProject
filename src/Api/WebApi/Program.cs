01c5eeec754d783413001e2afb4471150f6f26ca
Cihan Sungur <sungur1988@gmail.com>
2022-07-03 20:21:26 +0300

    PersistenceServiceRegistrationAdded

--- src/Api/WebApi/Program.cs
+++ src/Api/WebApi/Program.cs
@@ -1,6 +1,12 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var configuration = builder.Configuration;
+
+
+builder.Services.AddPersistence(configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
