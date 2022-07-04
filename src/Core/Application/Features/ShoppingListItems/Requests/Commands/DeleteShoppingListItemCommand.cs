5f5189194edd04ae6b6540a36f7227ca3ae33307
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 12:50:16 +0300

    DeleteShoppingListItemCommandCreated

---
+++ src/Core/Application/Features/ShoppingListItems/Requests/Commands/DeleteShoppingListItemCommand.cs
@@ -0,0 +1,7 @@
+﻿using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.ShoppingListItems.Requests.Commands
+{
+    public record DeleteShoppingListItemCommand(string Id) : IRequest<BaseResponse>;
+}
