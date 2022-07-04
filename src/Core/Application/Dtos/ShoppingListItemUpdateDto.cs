4e0904201d41c6da1d09fe653f67a1233d74e559
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 12:37:18 +0300

    ShoppingListItemUpdateDtoCreated

---
+++ src/Core/Application/Dtos/ShoppingListItemUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class ShoppingListItemUpdateDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsPurchased { get; set; }
+        public int Amount { get; set; }
+    }
+}
