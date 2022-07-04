b38b92b0d0345d45beac5adbf39703c5b8aa0b08
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 18:57:36 +0300

    DeleteCategoryCommandHandlerCreated

---
+++ src/Core/Application/Features/Categories/Handlers/Commands/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Constants;
+using Application.Contracts.Repositories.ReadRepositories;
+using Application.Contracts.Repositories.WriteRepositories;
+using Application.Features.Categories.Requests.Commands;
+using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.Categories.Handlers.Commands
+{
+    public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand, BaseResponse>
+    {
+        private readonly ICategoryReadRepository _categoryReadRepository;
+        private readonly ICategoryWriteRepository _categoryWriteRepository;
+
+        public DeleteCategoryCommandHandler(ICategoryWriteRepository categoryWriteRepository, ICategoryReadRepository categoryReadRepository)
+        {
+            _categoryWriteRepository = categoryWriteRepository;
+            _categoryReadRepository = categoryReadRepository;
+        }
+
+        public async Task<BaseResponse> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var entityToDelete = await _categoryReadRepository.GetById(request.Id);
+            if (entityToDelete==null)
+            {
+                return new BaseResponse(false,404,Messages.CategoryNotFound);
+            }
+            _categoryWriteRepository.Remove(entityToDelete);
+            return new BaseResponse(true, 200, Messages.CategoryDeleted);
+        }
+    }
+}
