8160f8a9725969529666349756249011a291ba3d
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 18:57:20 +0300

    DeleteCategoryCommandCreated

---
+++ src/Core/Application/Features/Categories/Requests/Commands/DeleteCategoryCommand.cs
@@ -0,0 +1,7 @@
+﻿using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.Categories.Requests.Commands
+{
+    public record DeleteCategoryCommand(string Id) : IRequest<BaseResponse>;
+}
