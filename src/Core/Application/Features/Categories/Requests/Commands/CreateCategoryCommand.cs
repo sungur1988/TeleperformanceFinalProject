6159c06b63d3dce6abf47882f28959b60f5429ea
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 13:02:21 +0300

    CreateCategoryCommandCreated

---
+++ src/Core/Application/Features/Categories/Requests/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos;
+using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.Categories.Requests.Commands
+{
+    public record CreateCategoryCommand(string Name) : IRequest<ServiceResponse<CategoryDto>>;
+}
