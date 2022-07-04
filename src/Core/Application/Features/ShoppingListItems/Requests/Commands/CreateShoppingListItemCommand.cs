2e8df52a70323ca1c568123ed0bcfebc4af6980f
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 12:39:39 +0300

    CreateShoppingListItemCommandsFolderChanged

---
+++ src/Core/Application/Features/ShoppingListItems/Requests/Commands/CreateShoppingListItemCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using Application.Wrapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ShoppingListItems.Requests.Commands
+{
+    public record CreateShoppingListItemCommand(string Name, string StockUnitId) : IRequest<ServiceResponse<ShoppingListItemDto>>;
+}
