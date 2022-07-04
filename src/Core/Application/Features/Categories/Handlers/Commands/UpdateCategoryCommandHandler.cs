54f15f41928b681387ec1b0c95d73ae36be1abd7
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 18:42:37 +0300

    UpdateCategoryCommandHandlerCreated

---
+++ src/Core/Application/Features/Categories/Handlers/Commands/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Constants;
+using Application.Contracts.Repositories.ReadRepositories;
+using Application.Contracts.Repositories.WriteRepositories;
+using Application.Dtos;
+using Application.Features.Categories.Requests.Commands;
+using Application.Wrapper;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Categories.Handlers.Commands 
+{
+    public class UpdateCategoryCommandHandler : IRequestHandler<UpdateCategoryCommand, ServiceResponse<CategoryDto>>
+    {
+        private readonly ICategoryReadRepository _categoryReadRepository;
+        private readonly ICategoryWriteRepository _categoryWriteRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateCategoryCommandHandler(ICategoryReadRepository categoryReadRepository, ICategoryWriteRepository categoryWriteRepository, IMapper mapper)
+        {
+            _categoryReadRepository = categoryReadRepository;
+            _categoryWriteRepository = categoryWriteRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ServiceResponse<CategoryDto>> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var entityToUpdate = await _categoryReadRepository.GetById(request.Id);
+            if (entityToUpdate==null)
+            {
+                return new ServiceResponse<CategoryDto>(default, false, 404, Messages.CategoryNotFound);
+            }
+            entityToUpdate = _mapper.Map(request, entityToUpdate);
+            _categoryWriteRepository.Update(entityToUpdate);
+            return new ServiceResponse<CategoryDto>(_mapper.Map<CategoryDto>(entityToUpdate), true, 200, Messages.CategoryUpdated);
+        }
+    }
+}
