152be0f7384e663d9f1619cf1eb51266ea9d3d3a
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 23:42:30 +0300

    GetAllCategoryQueryCreated

---
+++ src/Core/Application/Features/Categories/Requests/Queries/GetAllCategoryQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos;
+using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.Categories.Requests.Queries
+{
+    public record GetAllCategoryQuery(int PageNumber,int PageSize) : IRequest<ServiceResponse<IEnumerable<CategoryDto>>>;
+}
