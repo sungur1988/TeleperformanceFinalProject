7692977b060901ce488a9ae140c7cb73788be765
Cihan Sungur <sungur1988@gmail.com>
2022-07-05 00:19:03 +0300

    ModifiedForEmptyList

--- src/Core/Application/Features/Categories/Handlers/Queries/GetAllCategoryQueryHandler.cs
+++ src/Core/Application/Features/Categories/Handlers/Queries/GetAllCategoryQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using Application.Contracts.Repositories.ReadRepositories;
+﻿using Application.Constants;
+using Application.Contracts.Repositories.ReadRepositories;
 using Application.Dtos;
 using Application.Features.Categories.Requests.Queries;
 using Application.Wrapper;
@@ -18,9 +19,13 @@ namespace Application.Features.Categories.Handlers.Queries
             _mapper = mapper;
         }
 
-        public  Task<ServiceResponse<IEnumerable<CategoryDto>>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+        public Task<ServiceResponse<IEnumerable<CategoryDto>>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
         {
-            var result =  _categoryReadRepository.GetAll().Skip((request.PageNumber-1)*request.PageSize).Take(request.PageSize).AsEnumerable();
+            var result = _categoryReadRepository.GetAll().Skip((request.PageNumber - 1) * request.PageSize).Take(request.PageSize).AsEnumerable();
+            if (result.Count() == 0)
+            {
+                return Task.FromResult(new ServiceResponseIEnumerable<CategoryDto>(default,false,404,Messages.CategoryNotFound));
+            }
             return Task.FromResult(new ServiceResponse<IEnumerable<CategoryDto>>(_mapper.Map<IEnumerable<CategoryDto>>(result), true, 200));
         }
     }
