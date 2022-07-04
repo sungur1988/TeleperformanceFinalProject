2ba067f39dc9e23a627209f6fe24704a4d074704
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 12:39:52 +0300

    UpdateShoppingListItemCommandCreated

---
+++ src/Core/Application/Features/ShoppingListItems/Requests/Commands/UpdateShoppingListItemCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos;
+using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.ShoppingListItems.Requests.Commands
+{
+    public record UpdateShoppingListItemCommand(string Id, string Name, bool IsPurchased, int Amount) : IRequest<ServiceResponse<ShoppingListItemUpdateDto>>;
+}
