4d9c28c9c48428c03cd1b5e5069a19cd5d9b803b
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 23:20:41 +0300

    GetCategoryByIdQueryCreated

---
+++ src/Core/Application/Features/Categories/Requests/Queries/GetCategoryByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos;
+using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.Categories.Requests.Queries
+{
+    public record GetCategoryByIdQuery(string Id) : IRequest<ServiceResponse<CategoryDto>>;
+}
