5794406ddcec59d1fee5061e02350db3ccdf13bd
Cihan Sungur <sungur1988@gmail.com>
2022-07-05 00:23:26 +0300

    GetAllShoppingListQueryCreated

---
+++ src/Core/Application/Features/ShoppingLists/Requests/Queries/GetAllShoppingListQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos;
+using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.ShoppingLists.Requests.Queries
+{
+    public record GetAllShoppingListQuery(int PageNumber,int PageSize): IRequest<ServiceResponse<IEnumerable<ShoppingListDto>>>;
+}
