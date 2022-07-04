69532998509b87f90a23c3062d208db2cd863287
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 18:41:43 +0300

    CategoryUpdatedFieldAdded

--- src/Core/Application/Constants/Messages.cs
+++ src/Core/Application/Constants/Messages.cs
@@ -4,14 +4,15 @@
     {
         public static string StockUnitCreated = "Stock unit created";
         public static string StockUnitNotFound = "Stock unit not found";
-        public static string InternalServerError = "An unexpected error has occurred, please try again later";
         public static string StockUnitUpdated = "Stock unit updated";
         public static string StockUnitDeleted = "Stock unit deleted";
-        public static string CategoryNotFound = "Category not found";
+        public static string InternalServerError = "An unexpected error has occurred, please try again later";
         public static string ShoppingListItemCreated = "Shopping list item created";
         public static string ShoppingListItemNotFound = "Shopping list item not found";
         public static string ShoppingListItemUpdated = "Shopping list item updated";
         public static string ShoppingListItemDeleted = "Shopping list item deleted";
         public static string CategoryCreated = "Category created";
+        public static string CategoryUpdated = "Category updated";
+        public static string CategoryNotFound = "Category not found";
     }
 }
