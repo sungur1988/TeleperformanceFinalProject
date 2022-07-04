7af7ab7a0a6fc44e7e2fda813f52e54a37483476
Cihan Sungur <sungur1988@gmail.com>
2022-07-05 00:02:52 +0300

    GetShoppingListByIdQueryCreated

---
+++ src/Core/Application/Features/ShoppingLists/Requests/Queries/GetShoppingListByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos;
+using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.ShoppingLists.Requests.Queries
+{
+    public record GetShoppingListByIdQuery(string Id) : IRequest<ServiceResponse<ShoppingListDto>>;
+}
