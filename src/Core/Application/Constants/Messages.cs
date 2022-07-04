bff1a0f29f286206a327be7786ae86a1d0f5663c
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 12:37:06 +0300

    ShoppingListItemUpdatedAndShoppingListItemNotFoundFieldsAdded

--- src/Core/Application/Constants/Messages.cs
+++ src/Core/Application/Constants/Messages.cs
@@ -7,7 +7,9 @@
         public static string InternalServerError = "An unexpected error has occurred, please try again later";
         public static string StockUnitUpdated = "Stock unit updated";
         public static string StockUnitDeleted = "Stock unit deleted";
-        public static string CategoryNotFound= "Category not found";
-        public static string ShoppingListItemCreated ="Shopping list item created";
+        public static string CategoryNotFound = "Category not found";
+        public static string ShoppingListItemCreated = "Shopping list item created";
+        public static string ShoppingListItemNotFound = "Shopping list item not found";
+        public static string ShoppingListItemUpdated = "Shopping list item updated";
     }
 }
