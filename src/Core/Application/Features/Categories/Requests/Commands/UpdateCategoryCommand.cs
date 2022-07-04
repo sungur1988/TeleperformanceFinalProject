0268310f96abde606fe575162ee1abafb3278f8b
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 18:42:27 +0300

    UpdateCategoryCommandCreated

---
+++ src/Core/Application/Features/Categories/Requests/Commands/UpdateCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos;
+using Application.Wrapper;
+using MediatR;
+
+namespace Application.Features.Categories.Requests.Commands
+{
+    public record UpdateCategoryCommand(string Id,string Name): IRequest<ServiceResponse<CategoryDto>>;
+}
