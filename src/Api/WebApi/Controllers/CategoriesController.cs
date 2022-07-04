7cd7f9f8a42c21343a8134c9c03228c26da0c04e
Cihan Sungur <sungur1988@gmail.com>
2022-07-04 13:01:13 +0300

    CategoriesControllerCreatedWtihCreateCategoryMethod

---
+++ src/Api/WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.Categories.Requests.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CategoriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+    }
+}
