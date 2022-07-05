519a21a5b32a73fba4e69000a80f09b54186b954
Cihan Sungur <sungur1988@gmail.com>
2022-07-05 13:54:50 +0300

    AppIdentityDbContextCreated

---
+++ Identity/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
