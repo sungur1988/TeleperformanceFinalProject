3b8ec9add238c252ca049d8cae1bcca7d2cb1745
Cihan Sungur <sungur1988@gmail.com>
2022-07-05 13:55:10 +0300

    IdentityServiceRegistrationCreated

---
+++ Identity/IdentityServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity
+{
+    public static class IdentityServiceRegistration
+    {
+        public static IServiceCollection AddIdentityService(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("IdentityConnectionString"));
+            });
+
+            services.AddIdentity<AppUser,IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+
+            return services;
+        }
+    }
+}
